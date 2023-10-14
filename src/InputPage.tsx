1f5002588c2744c6fbe17f43bbe9ea08cb1b1da3
sidharth-n <sidharthnofficial@gmail.com>
2023-10-14 15:30:29 +0530

    svg changed

--- src/InputPage.tsx
+++ src/InputPage.tsx
@@ -41,22 +41,18 @@ const InputPage: React.FC<{ onPaste: (text: string) => void }> = ({
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="22"
-            height="22"
+            width="24"
+            height="24"
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
             stroke-width="2"
             stroke-linecap="round"
             stroke-linejoin="round"
-            class="lucide lucide-clipboard-list"
+            class="lucide lucide-power"
           >
-            <rect width="8" height="4" x="8" y="2" rx="1" ry="1" />
-            <path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2" />
-            <path d="M12 11h4" />
-            <path d="M12 16h4" />
-            <path d="M8 11h.01" />
-            <path d="M8 16h.01" />
+            <path d="M18.36 6.64a9 9 0 1 1-12.73 0" />
+            <line x1="12" x2="12" y1="2" y2="12" />
           </svg>
         </button>
       </div>
