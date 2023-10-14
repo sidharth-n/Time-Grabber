7076727ed0a567e642e34863293453fcc12c7676
sidharth-n <sidharthnofficial@gmail.com>
2023-10-14 16:05:57 +0530

    add component seconds left

---
+++ src/components/SecondsLeft.tsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+
+interface SecondsLeftProps {
+  currentAge: number;
+  maxAge: number;
+}
+
+const SecondsLeft: React.FC<SecondsLeftProps> = ({ currentAge, maxAge }) => {
+  const calculateSecondsLeft = () => {
+    const yearsLeft = maxAge - currentAge;
+    return yearsLeft * 365.25 * 24 * 60 * 60; // considering leap years as well
+  };
+
+  const [secondsLeft, setSecondsLeft] = useState(calculateSecondsLeft);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSecondsLeft((prevSeconds) => prevSeconds - 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return <>{secondsLeft.toLocaleString()}</>; // using toLocaleString to add commas for better readability
+};
+
+export default SecondsLeft;
