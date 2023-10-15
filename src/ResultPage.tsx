043c321230a29ba1d1f200adfbd07b4f96a1bfbd
sidharth-n <sidharthnofficial@gmail.com>
2023-10-15 07:41:21 +0530

    convert flex to grid

--- src/ResultPage.tsx
+++ src/ResultPage.tsx
@@ -31,75 +31,78 @@ const ResultPage: React.FC<{ age: number }> = ({ age }) => {
           </dl>
         </div>
       </li>
-
-      <a className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring">
-        <h2 className="mt-2 font-bold">Current age</h2>
-        <div className="flow-root mt-3">
-          <dl className="-my-3 divide-y divide-gray-100 text-sm">{age}</dl>
-        </div>
-      </a>
-      <a className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring">
-        <h2 className="mt-2 font-bold">Years left</h2>
-        <div className="flow-root mt-3">
-          <dl className="-my-3 divide-y divide-gray-100 text-sm">{ageLeft}</dl>
-        </div>
-      </a>
-      <a className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring">
-        <h2 className="mt-2 font-bold">Weeks left</h2>
-        <div className="flow-root mt-3">
-          <dl className="-my-3 divide-y divide-gray-100 text-sm">
-            <WeeksLeft timeLeft={ageLeft} />
-          </dl>
-        </div>
-      </a>
-      <a className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring">
-        <h2 className="mt-2 font-bold">Days left</h2>
-        <div className="flow-root mt-3">
-          <dl className="-my-3 divide-y divide-gray-100 text-sm">
-            <DaysLeft timeLeft={ageLeft} />
-          </dl>
-        </div>
-      </a>
-      <a className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring">
-        <h2 className="mt-2 font-bold">Minutes left</h2>
-        <div className="flow-root mt-3">
-          <dl className="-my-3 divide-y divide-gray-100 text-sm">
-            <MinutesLeft timeLeft={ageLeft} />
-          </dl>
-        </div>
-      </a>
-      <a className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring">
-        <h2 className="mt-2 font-bold">Seconds left</h2>
-        <div className="flow-root mt-3">
-          <dl className="-my-3 divide-y divide-gray-100 text-sm">
-            <SecondsLeft timeLeft={ageLeft} />
-          </dl>
-        </div>
-      </a>
-      <a className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring">
-        <h2 className="mt-2 font-bold">Heartbeats left</h2>
-        <div className="flow-root mt-3">
-          <dl className="-my-3 divide-y divide-gray-100 text-sm">
-            <BeatsLeft timeLeft={ageLeft} />
-          </dl>
-        </div>
-      </a>
-      <a className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring">
-        <h2 className="mt-2 font-bold">Sunrises left</h2>
-        <div className="flow-root mt-3">
-          <dl className="-my-3 divide-y divide-gray-100 text-sm">
-            <DaysLeft timeLeft={ageLeft} />
-          </dl>
-        </div>
-      </a>
-      <a className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring">
-        <h2 className="mt-2 font-bold">Breaths left</h2>
-        <div className="flow-root mt-3">
-          <dl className="-my-3 divide-y divide-gray-100 text-sm">
-            <BreathsLeft timeLeft={ageLeft} />
-          </dl>
-        </div>
-      </a>
+      <div className="grid grid-cols-2 gap-4">
+        <a className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring">
+          <h2 className="mt-2 font-bold">Current age</h2>
+          <div className="flow-root mt-3">
+            <dl className="-my-3 divide-y divide-gray-100 text-sm">{age}</dl>
+          </div>
+        </a>
+        <a className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring">
+          <h2 className="mt-2 font-bold">Years left</h2>
+          <div className="flow-root mt-3">
+            <dl className="-my-3 divide-y divide-gray-100 text-sm">
+              {ageLeft}
+            </dl>
+          </div>
+        </a>
+        <a className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring">
+          <h2 className="mt-2 font-bold">Weeks left</h2>
+          <div className="flow-root mt-3">
+            <dl className="-my-3 divide-y divide-gray-100 text-sm">
+              <WeeksLeft timeLeft={ageLeft} />
+            </dl>
+          </div>
+        </a>
+        <a className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring">
+          <h2 className="mt-2 font-bold">Days left</h2>
+          <div className="flow-root mt-3">
+            <dl className="-my-3 divide-y divide-gray-100 text-sm">
+              <DaysLeft timeLeft={ageLeft} />
+            </dl>
+          </div>
+        </a>
+        <a className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring">
+          <h2 className="mt-2 font-bold">Minutes left</h2>
+          <div className="flow-root mt-3">
+            <dl className="-my-3 divide-y divide-gray-100 text-sm">
+              <MinutesLeft timeLeft={ageLeft} />
+            </dl>
+          </div>
+        </a>
+        <a className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring">
+          <h2 className="mt-2 font-bold">Seconds left</h2>
+          <div className="flow-root mt-3">
+            <dl className="-my-3 divide-y divide-gray-100 text-sm">
+              <SecondsLeft timeLeft={ageLeft} />
+            </dl>
+          </div>
+        </a>
+        <a className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring">
+          <h2 className="mt-2 font-bold">Heartbeats left</h2>
+          <div className="flow-root mt-3">
+            <dl className="-my-3 divide-y divide-gray-100 text-sm">
+              <BeatsLeft timeLeft={ageLeft} />
+            </dl>
+          </div>
+        </a>
+        <a className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring">
+          <h2 className="mt-2 font-bold">Sunrises left</h2>
+          <div className="flow-root mt-3">
+            <dl className="-my-3 divide-y divide-gray-100 text-sm">
+              <DaysLeft timeLeft={ageLeft} />
+            </dl>
+          </div>
+        </a>
+        <a className="block rounded-xl border border-gray-100 p-4 shadow-sm hover:border-gray-200 hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring">
+          <h2 className="mt-2 font-bold">Breaths left</h2>
+          <div className="flow-root mt-3">
+            <dl className="-my-3 divide-y divide-gray-100 text-sm">
+              <BreathsLeft timeLeft={ageLeft} />
+            </dl>
+          </div>
+        </a>
+      </div>
     </div>
   );
 };
